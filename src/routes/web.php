2302e34a8db677889c3b59e02dc067e8e282c272
cameron macfarlane <cammac1984@gmail.com>
2017-11-08 17:27:40 -0800

    fixed route with out auth guest

--- src/routes/web.php
+++ src/routes/web.php
@@ -5,7 +5,7 @@ Route::group(['middleware' => ['web', 'auth', 'role:admin']], function () {
 	Route::post('admin/invitations/{id}/resend', 'Jameron\Invitations\Http\Controllers\Invitations\InvitationsController@resendInvitation');
 });
 
-Route::group(['middleware' => 'web', 'before' => 'auth'], function () {
+Route::group(['middleware' => ['web','guest']], function () {
     Route::get('register/{token}', 'Jameron\Invitations\Http\Controllers\Invitations\InvitationsController@getRegister');
     Route::post('invitations/register', 'Jameron\Invitations\Http\Controllers\Auth\InvitationRegisterController@register');
 });
